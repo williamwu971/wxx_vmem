aa3508e1f7026647a2a05c646f53090e9862f8d4
William <xiwu9085@gmail.com>
2023-03-03 20:00:56 +1100

    -AUTO- Fri Mar  3 08:00:56 PM AEDT 2023

--- src/libvmmalloc/libvmmalloc.c
+++ src/libvmmalloc/libvmmalloc.c
@@ -420,6 +420,8 @@ libvmmalloc_create(const char *dir, size_t size)
 		return NULL;
 	}
 
+    printf("reached mmap, prefaulting pointer %p of size %.2fgb\n"addr,(double)size/1024./1024./1024.);
+
 	/* store opaque info at beginning of mapped area */
 	struct vmem *vmp = addr;
 	memset(&vmp->hdr, '\0', sizeof(vmp->hdr));
